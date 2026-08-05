c0475bef1e0b702a679bbdd973c69fc81f5ab6df
vnnrn2hfjw-max <vnnrn2hfjw@privaterelay.appleid.com>
2026-08-05 12:13:35 +0300

    Update ready.js

--- Events/ready.js
+++ Events/ready.js
@@ -1,1 +1,62 @@
+// ======================
+// 🚀 READY EVENT
+// ======================
 
+module.exports = {
+
+  name: "ready",
+
+  once: true,
+
+
+  execute(client) {
+
+
+    console.log(
+      "================================="
+    );
+
+
+    console.log(
+      `🔴 NSC Bot Online: ${client.user.tag}`
+    );
+
+
+    console.log(
+      `👥 Servers: ${client.guilds.cache.size}`
+    );
+
+
+    console.log(
+      "⚫ No Second Chances System Loaded"
+    );
+
+
+    console.log(
+      "================================="
+    );
+
+
+    client.user.setPresence({
+
+      activities: [
+
+        {
+
+          name: "NSC | No Second Chances",
+
+          type: 3
+
+        }
+
+      ],
+
+
+      status: "online"
+
+    });
+
+
+  }
+
+};
