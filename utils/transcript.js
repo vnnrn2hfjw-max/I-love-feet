f927ecc1f0ff7c760e8100247367b714ffc9b837
vnnrn2hfjw-max <vnnrn2hfjw@privaterelay.appleid.com>
2026-08-06 10:21:54 +0300

    Update transcript.js

--- utils/transcript.js
+++ utils/transcript.js
@@ -1,1 +1,52 @@
+const {
+    AttachmentBuilder
+} = require("discord.js");
 
+
+async function createTranscript(channel) {
+
+    const messages =
+        await channel.messages.fetch({
+            limit: 100
+        });
+
+
+    const sorted =
+        messages
+        .sort(
+            (a,b) =>
+            a.createdTimestamp -
+            b.createdTimestamp
+        );
+
+
+    let text =
+    `Transcript: ${channel.name}\n\n`;
+
+
+    for (const message of sorted.values()) {
+
+        text +=
+        `${message.author.tag}: ${message.content}\n`;
+
+    }
+
+
+    const buffer =
+        Buffer.from(text, "utf-8");
+
+
+    return new AttachmentBuilder(
+        buffer,
+        {
+            name:
+            `${channel.name}-transcript.txt`
+        }
+    );
+
+}
+
+
+module.exports = {
+    createTranscript
+};
