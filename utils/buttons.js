6224b0cd62f5d9d469dc454555901c98bd58ec6c
vnnrn2hfjw-max <vnnrn2hfjw@privaterelay.appleid.com>
2026-08-06 10:25:51 +0300

    Add button interaction handler for tickets
    Handle button interactions for ticket claiming and closing.

--- utils/buttons.js
+++ utils/buttons.js
@@ -1,1 +1,38 @@
+module.exports = async function handleButton(interaction) {
 
+
+    if (
+        interaction.customId === "ticket_claim"
+    ) {
+
+        const claim =
+            require("./claimButton");
+
+
+        return claim(
+            interaction
+        );
+
+    }
+
+
+
+    if (
+        interaction.customId === "ticket_close"
+    ) {
+
+        const close =
+            require("./closeTicket");
+
+
+        return close(
+            interaction
+        );
+
+    }
+
+
+
+    return;
+
+};
